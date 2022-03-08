213ee9c02fd695f9a17c51beaa89686ac5767d61
Daniel Holland <danhol1989@gmail.com>
2022-03-08 13:28:33 -0800

    Deleted inquirer.js, and added code to server.js. Added more to server.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,26 @@ CREATE DATABASE employee_db;
 
 use employee_db;
 
-create Table employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) not null,
-    last_name VARCHAR(30) not null,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL,
-    manager_name VARCHAR(30)
-);
-
-create table role(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) not null,
-    salary DECIMAL not null,
-    FOREIGN key (department_id) 
-    references department(id) on delete set null
-);
-
 create table department (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) Not Null
+);
+
+create table role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) not null,
+    salary DECIMAL not null
+    -- FOREIGN key (department_id) 
+    -- references department(id) 
+    -- on delete set null
+);
+
+create Table employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) not null,
+    last_name VARCHAR(30) not null
+    -- FOREIGN KEY (role_id)
+    -- REFERENCES role(id)
+    -- ON DELETE SET NULL,
+    -- manager_name VARCHAR(30)
 );
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const consoleTable = require('console.table');
+const cTable = require('console.table');
+const express = require('express');
 const sequelize = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,5 +14,96 @@ app.use(express.json());
   // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-});
+}).then(startApp());
 
+function startApp() {
+  console.log("***********************************")
+  console.log("*                                 *")
+  console.log("*        EMPLOYEE MANAGER         *")
+  console.log("*                                 *")
+  console.log("***********************************")
+
+  initPrompt();
+
+  function initPrompt() {
+      inquirer.prompt([
+          {
+              message: 'Please select from the following:',
+              type: 'list',
+              name: 'options',
+              choices: ['View all departments', 
+                        'View all roles', 
+                        'View all employees', 
+                        'Add a department', 
+                        'Add a role', 
+                        'Add an employee', 
+                        'Update an employee role',
+                        'Exit'] 
+          }
+      ]).then((choices) => {
+          const choice = choices.options;
+
+          if (choice === "View all departments") {
+              showDepts();
+          } 
+          if (choice === "View all roles") {
+              showRoles();
+          } 
+          if (choice === "View all employees") {
+              showEmployees();
+          } 
+          if (choice === "Add a department") {
+              addDept();
+          } 
+          if (choice === "Add a role") {
+              addRole();
+          } 
+          if (choice === "Add an employee") {
+              addEmployee();
+          } 
+          if (choice === "Update an employee role") {
+              updateEmployee();
+          }  
+          if (choice === "Exit") {
+            console.log("Now Exiting Program");
+            process.exit(1);
+        }    
+      });
+  };
+
+  function showDepts() {
+      // console.table('employee_db', [...]);
+
+      initPrompt();
+  };
+
+  function showRoles() {
+
+      initPrompt();
+  };
+
+  function showEmployees() {
+
+      initPrompt();
+  };
+
+  function addDept() {
+
+      initPrompt();
+  };
+
+  function addRole() {
+
+      initPrompt();
+  };
+
+  function addEmployee() {
+
+      initPrompt();
+  };
+
+  function updateEmployee() {
+
+      initPrompt();
+  };
+};
